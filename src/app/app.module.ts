24c50bd965ea5b37f6e085028cb3d6be9c789a93
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-04 17:24:29 +0200

    add my works

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubComponent,
+    GithubFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
