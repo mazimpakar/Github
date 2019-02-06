5910f348bf0e8377eae82d12681f05ecd894b322
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-06 11:11:37 +0200

    add link in component

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -15,15 +15,15 @@ export class GithubFormComponent implements OnInit {
   ngOnInit() {}
 }
 
-// import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
-// // import { User } from "../../models/user.model";
-// @Component({
-//   selector: "app-user",
-//   templateUrl: "./user.component.html",
-//   styleUrls: ["./user.component.css"],
-//   changeDetection: ChangeDetectionStrategy.OnPush
-// })
-// export class UserComponent {
-//   @Input()
-//   user: User;
-// }
+import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
+// import { User } from "../../models/user.model";
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserComponent {
+  @Input()
+  user: User;
+}
