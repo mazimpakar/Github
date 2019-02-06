6e81e1bb09fe5ea139eff756f4331be53ace1082
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-06 11:05:07 +0200

    add form component.ts

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -1,19 +1,19 @@
-// import { Component, OnInit } from "@angular/core";
-// import { Github } from "../github";
+import { Component, OnInit } from "@angular/core";
+import { Github } from "../github";
 
-// @Component({
-//   selector: "app-github-form",
-//   templateUrl: "./github-form.component.html",
-//   styleUrls: ["./github-form.component.css"]
-// })
-// export class GithubFormComponent implements OnInit {
-//   // newUser = new User(0, "");
-//   newUserName: string;
-//   theUser: any;
-//   constructor() {}
+@Component({
+  selector: "app-github-form",
+  templateUrl: "./github-form.component.html",
+  styleUrls: ["./github-form.component.css"]
+})
+export class GithubFormComponent implements OnInit {
+  // newUser = new User(0, "");
+  newUserName: string;
+  theUser: any;
+  constructor() {}
 
-//   ngOnInit() {}
-// }
+  ngOnInit() {}
+}
 
 import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 // import { User } from "../../models/user.model";
